0cb74c9af4db76d0aa11e655b4ec668a5fa92fc6
jmaicas <jorgemaicas@gmail.com>
2021-04-05 15:44:58 +0100

    Add files via upload

--- reading_analog_port.py
+++ reading_analog_port.py
@@ -1,8 +1,15 @@
 import pyfirmata
 import numpy as np
 
-pin = 1
-sampling_time = 0.1 # in seconds
+pin = 1 # analog pin to receive the obstacle info
+pin_out = 12 # digital pin to send the trigger info
+time_min = 60
+total_time = time_min*60 # in seconds
+
+# Parameters to follow the protocol in Carlezon & Chartoff (2007)
+stim_len = 0.5
+sampling_freq = 141 # Hz
+sampling_time = 1/sampling_freq # in seconds
 
 # Creates a new board 
 board = pyfirmata.Arduino('/dev/ttyACM0')
@@ -12,20 +19,26 @@ it.start()
  
 # Start reporting for define pin
 board.analog[pin].enable_reporting()
-
  
-# Loop for reading the input. Duration approx. 10 s
-for i in np.arange(1, 1000, sampling_time):
+# Run until the total time is reached
+counter = 0
+for i in np.arange(0, total_time, sampling_time):
     #print("\n Checking state at second %f" % i)
     #print("Pin %i : %s" % (pin, board.analog[pin].read()))
-    if board.analog[1].read() is None:
+    if board.analog[pin].read() is None:
       print("Pin with no value")
     else:
-      if board.analog[1].read() > 0.75:        
-        board.digital[12].write(1)
-        print('obstacle!')
+      if board.analog[pin].read() > 0.75:
+        if counter < stim_len:        
+          board.digital[pin_out].write(1)
+          print('obstacle and signal!')
+        else: 
+          board.digital[pin_out].write(0)
+          print('    obstacle')
+        counter = counter + sampling_time
       else:
-        board.digital[12].write(0)
+        board.digital[pin_out].write(0)
+        counter = 0
         print('')
     board.pass_time(sampling_time)
  
