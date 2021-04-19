8ffd59c8e9f372630e6044608de0797f201e5c50
jmaicas <jorgemaicas@gmail.com>
2021-04-19 14:01:13 +0100

    extra presence detector added and outputs for leds

--- reading_analog_port.py
+++ reading_analog_port.py
@@ -5,7 +5,11 @@ import xlsxwriter
 #from seaborn.palettes import cubehelix_palette
 
 pin = 1 # analog pin to receive the obstacle info
+pin2 = 2 # analog pin to receive the second obstacle info
 pin_out = 12 # digital pin to send the trigger info
+pin_outduplicated = 10
+pin2_out = 8 # digital pin to send the trigger info
+pin2_outduplicated = 6 # digital pin to send the trigger info
 time_min = 5 # time in minutes to run the experiment
 total_time = time_min*60 # in seconds
 delay_time = 0  # delay after stimulus 0 is not delay
@@ -17,6 +21,13 @@ sampling_freq = 20 #141 # Hz
 sampling_time = 1/sampling_freq # in seconds
 half_sampling = sampling_time/2
 
+# Creating an input for tests
+# random 
+n = 100000
+m = 100000
+a = np.hstack((np.ones(n), np.zeros(m)))
+np.random.shuffle(a)
+
 # Creates a new board 
 board = pyfirmata.Arduino('COM7') # Windows
 #board = pyfirmata.Arduino('/dev/ttyACM0') # Linux
@@ -26,15 +37,20 @@ it.start()
  
 # Start reporting for define pin
 board.analog[pin].enable_reporting()
+board.analog[pin2].enable_reporting()
  
 # Run until the total time is reached
 counter = 0
+counter2 = 0
 count_betw_interv = 5
 
 delay_after_stimulus = delay_time
 delay_stimulus = False
 keep_stimulus = False
 took_nose_out = True
+delay_stimulus2 = False
+keep_stimulus2 = False
+took_nose_out2 = True
 stim_times = np.zeros(int(total_time/sampling_time))
 c = 0
 
@@ -50,12 +66,14 @@ for i in np.arange(0, total_time, sampling_time):
         
         if counter < stim_len and took_nose_out and count_betw_interv > thres_betw_interv:
           board.digital[pin_out].write(1)
+          board.digital[pin_outduplicated].write(1)
           keep_stimulus = True    
           count_betw_interv = 0
           #stim_times[c] = 1
         elif counter > stim_len:
           took_nose_out = False
           board.digital[pin_out].write(0)
+          board.digital[pin_outduplicated].write(0)
           count_betw_interv = count_betw_interv + sampling_time
           counter = 0
           keep_stimulus = False
@@ -70,15 +88,58 @@ for i in np.arange(0, total_time, sampling_time):
         count_betw_interv = count_betw_interv + sampling_time
         if counter < stim_len and keep_stimulus:
           board.digital[pin_out].write(1)
+          board.digital[pin_outduplicated].write(1)
           counter = counter + sampling_time
           #stim_times[c] = 1
         else:
           took_nose_out = True
           counter = 0
           board.digital[pin_out].write(0)
+          board.digital[pin_outduplicated].write(0)
           keep_stimulus = False
     else:
       print("Pin with no value")
+
+    # Analysis for the detector that does not give reinforcement
+    if board.analog[pin2].read() is not None:
+      
+      if (board.analog[pin2].read() > 0.75):
+        
+        if counter2 < stim_len and took_nose_out2 and count_betw_interv2 > thres_betw_interv:
+          board.digital[pin2_out].write(1)
+          board.digital[pin2_outduplicated].write(1)
+          keep_stimulus2 = True    
+          count_betw_interv2 = 0
+          #stim_times[c] = 1
+        elif counter2 > stim_len:
+          took_nose_out2 = False
+          board.digital[pin2_out].write(0)
+          board.digital[pin2_outduplicated].write(0)
+          count_betw_interv2 = count_betw_interv2 + sampling_time
+          counter2 = 0
+          keep_stimulus2 = False
+        elif count_betw_interv2 <= thres_betw_interv:
+          took_nose_out2 = False
+          count_betw_interv2 = count_betw_interv2 + sampling_time
+
+          
+        counter2 = counter2 + sampling_time
+
+      else:
+        count_betw_interv2 = count_betw_interv2 + sampling_time
+        if counter2 < stim_len and keep_stimulus2:
+          board.digital[pin2_out].write(1)
+          board.digital[pin2_outduplicated].write(0)
+          counter2 = counter2 + sampling_time
+          #stim_times[c] = 1
+        else:
+          took_nose_out2 = True
+          counter2 = 0
+          board.digital[pin2_out].write(0)
+          board.digital[pin2_outduplicated].write(0)
+          keep_stimulus2 = False
+    else:
+      print("Pin 2 with no value")
 
     board.pass_time(sampling_time)
 
